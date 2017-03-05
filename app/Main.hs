03b64aa52f8f3714a66e9b0ad26beff101055935
Guillaume Lasnier <guillaume.lasnier@me.com>
2017-03-05 19:57:17 +0100

    Add Single Character Tokenizer

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import Calculator.Lexer
 
 main :: IO ()
-main = print $ operator '*'
+main = print $ tokenize " 1 + 4 / x "
--- src/Calculator/Lexer.hs
+++ src/Calculator/Lexer.hs
@@ -6,6 +6,8 @@ module Calculator.Lexer
   tokenize
 ) where
 
+import Data.Char
+
 data Operator = Plus | Minus | Times | Div
     deriving (Show, Eq)
 
@@ -32,4 +34,9 @@ operator c | c == '+' = Plus
            | c == '/' = Div
 
 tokenize :: String -> [Token]
-tokenize = undefined
+tokenize [] = []
+tokenize (c : cs) | elem c "+-*/" = TokOp (operator c) : tokenize cs
+                  | isDigit c  = TokNum (digitToInt c) : tokenize cs
+                  | isAlpha c  = TokIdent [c]          : tokenize cs
+                  | isSpace c  = tokenize cs
+                  | otherwise  = error $ "Cannot tokenize " ++ [c]
