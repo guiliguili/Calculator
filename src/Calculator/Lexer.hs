16d440846fcacc03631b0a87b358d147f433aa56
Guillaume Lasnier <guillaume.lasnier@me.com>
2017-03-26 11:32:20 +0200

    Implement Mulitple Character Tokenizer for any sentence

--- src/Calculator/Lexer.hs
+++ src/Calculator/Lexer.hs
@@ -13,7 +13,6 @@ data Operator = Plus | Minus | Times | Div
 data Token = TokOp Operator
               | TokIdent String
               | TokNum Int
-              | TokSpace
                deriving (Show, Eq)
 -- show
 showContent :: Token -> String
@@ -64,3 +63,6 @@ tokenize ::  String -> [Token]
 tokenize [] = []
 tokenize (c : cs) | isAlpha c = identifier c cs
                   | isDigit c = number c cs
+                  | isSpace c = tokenize cs
+                  | c `elem` "+-*/" = TokOp (operator c) : tokenize cs
+                  | otherwise = error $ "Cannot tokenize " ++ [c]
