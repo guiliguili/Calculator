5f446406d198e617743b09624edb2c3accd11f4c
Guillaume Lasnier <guillaume.lasnier@me.com>
2017-04-07 14:13:00 +0200

    Prepare Threading the Symbol Table

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,25 @@
 module Main where
 
+import qualified Data.Map as M
+
 import Calculator.Lexer
 import Calculator.Parser
 import Calculator.Evaluator
 
 main :: IO ()
 main = do
+  loop (M.fromList [("pi", pi)])
+
+loop symTab = do
   str <- getLine
-  do
-    if null str
-     then
-        return ()
-     else
-        do
-          print $ evaluate $ parse $ tokenize str
-          main
+  if null str
+  then
+    return ()
+  else
+    let
+      toks = tokenize str
+      tree = parse toks
+      (val, symTab') = evaluate tree symTab
+    in do
+      print val
+      loop symTab'
--- src/Calculator/Evaluator.hs
+++ src/Calculator/Evaluator.hs
@@ -3,30 +3,12 @@ module Calculator.Evaluator
   evaluate
 ) where
 
+import qualified Data.Map as M
+
 import Calculator.Lexer
 import Calculator.Parser
 
-evaluate :: Tree -> Double
-evaluate (SumNode op left right) =
-    let lft = evaluate left
-        rgt = evaluate right
-    in
-        case op of
-          Plus  -> lft + rgt
-          Minus -> lft - rgt
+type SymTab = M.Map String Double
 
-evaluate (ProdNode op left right) =
-    let lft = evaluate left
-        rgt = evaluate right
-    in
-        case op of
-          Times -> lft * rgt
-          Div   -> lft / rgt
-
-evaluate (UnaryNode op tree) =
-    let x = evaluate tree
-    in case op of
-         Plus  -> x
-         Minus -> -x
-
-evaluate (NumNode x) = x
+evaluate :: Tree -> SymTab -> (Double, SymTab)
+evaluate = undefined
