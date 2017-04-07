e3573fd5e45d75383d43af6d895c59d21a3a522c
Guillaume Lasnier <guillaume.lasnier@me.com>
2017-04-07 20:05:51 +0200

    Threading the Symbol Table

--- src/Calculator/Evaluator.hs
+++ src/Calculator/Evaluator.hs
@@ -10,5 +10,50 @@ import Calculator.Parser
 
 type SymTab = M.Map String Double
 
+lookUp :: String -> SymTab -> (Double, SymTab)
+lookUp str symTab =
+  case M.lookup str symTab of
+    Just v -> (v, symTab)
+    Nothing -> error $ "Undefined variable " ++ str
+
+addSymbol :: String -> Double -> SymTab -> ((), SymTab)
+addSymbol str val symTab =
+  let symTab' = M.insert str val symTab
+  in ((), symTab')
+
 evaluate :: Tree -> SymTab -> (Double, SymTab)
-evaluate = undefined
+
+evaluate (SumNode op left right) symTab =
+  let
+    (lft, symTab') = evaluate left symTab
+    (rgt, symTab'') = evaluate right symTab'
+  in
+    case op of
+      Plus  -> (lft + rgt, symTab'')
+      Minus -> (lft - rgt, symTab'')
+
+evaluate (ProdNode op left right) symTab =
+  let
+    (lft, symTab') = evaluate left symTab
+    (rgt, symTab'') = evaluate right symTab'
+  in
+    case op of
+      Times -> (lft * rgt, symTab)
+      Div   -> (lft / rgt, symTab)
+
+evaluate (UnaryNode op tree) symTab =
+  let (x, symTab') = evaluate tree symTab
+  in
+    case op of
+      Plus  -> (x, symTab')
+      Minus -> (-x, symTab')
+
+evaluate (NumNode x) symTab = (x, symTab)
+
+evaluate (VarNode str) symTab = lookUp str symTab
+
+evaluate (AssignNode str tree) symTab =
+  let
+    (v, symTab')  = evaluate tree symTab
+    (_, symTab'') = addSymbol str v symTab'
+  in (v, symTab'')
